9fdf2b7a89a84d264ea9e201528f9401de78d976
Oleh Stasiv <olegstasiw@icloud.com>
2023-03-22 20:34:59 +0200

    Initial commit

---
+++ IMDb Top Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDb Top Movies
+//
+//  Created by Oleh Stasiv on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
