44548dc30d3151b30b56cfd46295fc217b0de8ba
olegstasiw <32603788+olegstasiw@users.noreply.github.com>
2023-03-22 21:14:37 +0200

    General project settings (#1)

---
+++ IMDb Top Movies/Supporting files/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  IMDb Top Movies
+//
+//  Created by Oleh Stasiv on 22.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
